20b65443bd8b4d44d3d12cfb862acf8c8bc2cbc6
Eliel Gordon <gordoneliel@gmail.com>
2017-11-29 03:40:28 -0800

    Add fetch results controller

--- MakeInventory/InventoriesViewController.swift
+++ MakeInventory/InventoriesViewController.swift
@@ -11,9 +11,31 @@ import CoreData
 
 class InventoriesViewController: UIViewController {
     let stack = CoreDataManger(name: CoreDataManagerName)
-    
+
     @IBOutlet weak var tableView: UITableView!
     var inventories = [Inventory]()
+    
+    lazy var fetchedResultsController: NSFetchedResultsController<Inventory> = {
+        // Initialize Fetch Request
+        let fetchRequest: NSFetchRequest<Inventory> = Inventory.fetchRequest()
+        
+        // Add Sort Descriptors
+        let sortDescriptor = NSSortDescriptor(key: "name", ascending: true)
+        fetchRequest.sortDescriptors = [sortDescriptor]
+        
+        // Initialize Fetched Results Controller
+        let fetchedResultsController = NSFetchedResultsController(
+            fetchRequest: fetchRequest,
+            managedObjectContext: self.stack.viewContext,
+            sectionNameKeyPath: nil,
+            cacheName: nil
+        )
+        
+        // Configure Fetched Results Controller
+        fetchedResultsController.delegate = self
+        
+        return fetchedResultsController
+    }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -24,18 +46,54 @@ class InventoriesViewController: UIViewController {
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         
-        let fetch = NSFetchRequest<Inventory>(entityName: "Inventory")
-        do {
-            let result = try stack.viewContext.fetch(fetch)
-            self.inventories = result
-            self.tableView.reloadData()
-            
-        }catch let error {
-            print(error)
-        }
+        try? fetchedResultsController.performFetch()
+        tableView.reloadData()
     }
 }
 
+extension InventoriesViewController: NSFetchedResultsControllerDelegate {
+    func controllerWillChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
+        tableView.beginUpdates()
+    }
+    
+    func controllerDidChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
+        tableView.endUpdates()
+    }
+    
+    func controller(_ controller: NSFetchedResultsController<NSFetchRequestResult>, didChange anObject: Any, at indexPath: IndexPath?, for type: NSFetchedResultsChangeType, newIndexPath: IndexPath?) {
+        switch (type) {
+        case .insert:
+            if let indexPath = newIndexPath {
+                tableView.insertRows(at: [indexPath], with: .fade)
+            }
+            break
+        case .delete:
+            if let indexPath = indexPath {
+                tableView.deleteRows(at: [indexPath], with: .fade)
+            }
+            break
+        case .update:
+            if let indexPath = indexPath, let cell = tableView.cellForRow(at: indexPath) {
+                
+                let item = fetchedResultsController.object(at: indexPath)
+                
+                cell.textLabel?.text = item.name
+                cell.detailTextLabel?.text = "x\(item.quantity)"
+                
+            }
+            break
+        case .move:
+            if let indexPath = indexPath {
+                tableView.deleteRows(at: [indexPath], with: .fade)
+            }
+            
+            if let newIndexPath = newIndexPath {
+                tableView.insertRows(at: [newIndexPath], with: .fade)
+            }
+            break
+        }
+    }
+}
 
 extension InventoriesViewController: UITableViewDataSource {
     func numberOfSections(in tableView: UITableView) -> Int {
@@ -43,7 +101,12 @@ extension InventoriesViewController: UITableViewDataSource {
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return inventories.count
+        guard let sections = fetchedResultsController.sections else {
+            return 0
+        }
+        
+        let sectionInfo = sections[section]
+        return sectionInfo.numberOfObjects
     }
 }
 
@@ -51,11 +114,22 @@ extension InventoriesViewController: UITableViewDelegate {
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "InventoryCell", for: indexPath)
         
-        let item = inventories[indexPath.row]
+        let item = fetchedResultsController.object(at: indexPath)
         
         cell.textLabel?.text = item.name
         cell.detailTextLabel?.text = "x\(item.quantity)"
         
         return cell
+    }
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        guard editingStyle == .delete else { return }
+        
+        // Fetch
+        let inventory = fetchedResultsController.object(at: indexPath)
+        
+        // Delete
+        fetchedResultsController.managedObjectContext.delete(inventory)
+        try? fetchedResultsController.managedObjectContext.save()
     }
 }
